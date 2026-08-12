75523b1311d80368fd159e5c94d3c8248e88e66e
nikarukovodstvo <nikarukovodstvo@gmail.com>
2026-08-12 19:49:37 +0300

    Add cmd/server/main.go

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/mnikitenkov/appchain-control-plane-demo/internal/configstore"
+)
+
+func main() {
+	address := os.Getenv("HTTP_ADDRESS")
+	if address == "" {
+		address = ":8080"
+	}
+	server := &http.Server{
+		Addr:              address,
+		Handler:           configstore.NewHandler(configstore.NewService()),
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+	log.Printf("appchain control plane listening on %s", address)
+	log.Fatal(server.ListenAndServe())
+}
+
